1206ae8e44b3b8fa3ebf6c4bfab2e373c7cfd93b
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-11-27 19:39:44 -0700

    Java Cafe Update

---
+++ CafeJava.java
@@ -0,0 +1,19 @@
+public class CafeJava{
+    public static void main(String[] args) {
+        // App Variable
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage =  ", your order will be ready shortly";
+        String readyMessage = ", your order is ready";
+        String displayTotalMessage = "Yout total is $";
+
+        // Menu Variable
+        double mochaPrice = 3.5;
+
+        // Customer name Variables
+        String customer1 = "Cindhuri";
+
+        // Order Completions
+        boolean isReadyOrder1  = false;
+        
+    }
+}
